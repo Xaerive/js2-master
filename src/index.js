525f32ab20d42e3db43e255065884de81b7dda36
Roman Rodionov <roman_rodionov@epam.com>
2022-04-11 19:30:48 +0300

    - fixed tasks addition - added done-section

--- src/index.js
+++ src/index.js
@@ -1,43 +1,54 @@
 import './style.css';
-const sectionTasks = document.querySelector('.open-section .tasks')
+const sectionTasks = document.querySelector('.open-section .tasks');
+const sectionTasksDone = document.querySelector('.done-section .tasks');
 
-const gogo = document.querySelector('.ADD')
+const gogo = document.querySelector('.ADD');
+const taskNameInput = document.querySelector('.new-task-section input');
 
-gogo.onclick = function () {
-console.log('hello')
+gogo.onclick = function() {
+  const newTask = document.createElement('div');
+  newTask.classList.add('task');
 
-const newTask = document.createElement('div')
-newTask.classList.add('task')
+  const labelCheckbox = document.createElement('label');
+  labelCheckbox.classList.add('checkbox');
 
+  const inputChekbox = document.createElement('input');
+  inputChekbox.classList.add('checkboxTask');
+  inputChekbox.setAttribute('type', 'checkbox');
+  inputChekbox.onchange = function() {
+    if (inputChekbox.checked) {
+      sectionTasks.removeChild(newTask);
+      sectionTasksDone.appendChild(newTask);
+    } else {
+      sectionTasksDone.removeChild(newTask);
+      sectionTasks.appendChild(newTask);
+    }
+  };
 
-const inputCheckboxTask = document.createElement('div')
-inputCheckboxTask.classList.add('inputCheckboxTask')
+  const divInputTextTask = document.createElement('div');
+  divInputTextTask.classList.add('titleTask');
+  divInputTextTask.textContent = taskNameInput.value;
 
+  const dateDiv = document.createElement('div');
+  const creationDateDiv = document.createElement('div');
+  creationDateDiv.classList.add('alltime');
+  const creationTimeElement = document.createElement('time');
+  creationTimeElement.textContent = new Date().toLocaleTimeString(undefined, {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+  creationDateDiv.appendChild(creationTimeElement);
+  dateDiv.appendChild(creationDateDiv);
+  // const inputTitleTask = document.createElement('input');
+  // inputTitleTask.classList.add('titleInputTask');
+  // inputTitleTask.setAttribute('type', 'text');
 
-const labelCheckbox = document.createElement('label')
-labelCheckbox.classList.add('checkbox')
+  labelCheckbox.appendChild(inputChekbox);
+  // divInputTextTask.appendChild(inputTitleTask);
 
-const inputChekbox = document.createElement('input')
-inputChekbox.classList.add('checkboxTask')
-inputChekbox.setAttribute('type', 'checkbox')
+  newTask.appendChild(labelCheckbox);
+  newTask.appendChild(divInputTextTask);
+  newTask.appendChild(dateDiv);
 
-
-const divInputTextTask = document.createElement('div')
-divInputTextTask.classList.add('titleTask')
-
-const inputTitleTask = document.createElement('input')
-inputTitleTask.classList.add('titleInputTask')
-inputTitleTask.setAttribute('type', 'text')
-
-
-labelCheckbox.appendChild(inputChekbox)
-divInputTextTask.appendChild(inputTitleTask)
-
-inputCheckboxTask.appendChild(labelCheckbox)
-inputCheckboxTask.appendChild(divInputTextTask)
-
-newTask.appendChild(inputCheckboxTask)
-
-sectionTasks.appendChild(newTask)
-
-}
+  sectionTasks.appendChild(newTask);
+};
